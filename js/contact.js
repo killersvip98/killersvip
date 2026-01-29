885c0c6b187db4b06d61a8d42b5b07a6fcdea0ee
killersvip98 <killersvip98@gmail.com>
2026-01-29 15:50:31 +0530

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,21 @@
+function copyText(text) {
+  navigator.clipboard.writeText(text);
+  alert("Copied: " + text);
+}
+
+
+function joinVipWhatsApp() {
+  window.location.href = "https://chat.whatsapp.com/JuGY8V91TpQInvIsae0Duc";
+}
+
+function joinVipTelegram() {
+  window.location.href = "https://t.me/+rZFGwHu2Lq0xYWU1";
+}
+
+function sendReceiptWhatsApp() {
+  window.location.href = "https://wa.me/729190799";
+}
+
+function sendReceiptTelegram() {
+  window.location.href = "https://t.me/Dila_vip1";
+}
