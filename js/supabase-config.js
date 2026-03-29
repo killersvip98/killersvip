f4effbf8a991697f058edb702f62794c1a2ece17
killersvip <killersvip98@gmail.com>
2026-03-29 13:09:20 +0530

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,7 +1,7 @@
 // Supabase Configuration Template
 // Replace with your real Supabase URL and Key
-const SUPABASE_URL = 'https://rcwerdnxvqztmcxcorhi.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjd2VyZG54dnF6dG1jeGNvcmhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNDM3MDEsImV4cCI6MjA4ODgxOTcwMX0.F57R8C2-ajW-C2vKvJRnVkbKb6erTbijkosYDcvf5dc';
+const SUPABASE_URL = 'https://ukujpotpbdxvlcvjhssa.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrdWpwb3RwYmR4dmxjdmpoc3NhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3Njc1NzgsImV4cCI6MjA5MDM0MzU3OH0.9uWUgWg63FVTLiYh-AA7YqXZnLQWRTlf1fDgmuTtxnI';
 
 const { createClient } = supabase;
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
